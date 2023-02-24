cf387fbecd376900a8b452412c1d1f3b30371901
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-24 19:58:24 +0500

    Add the methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, inverse_of: :author
+  has_many :comments, inverse_of: :author
+  has_many :likes, inverse_of: :author
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
