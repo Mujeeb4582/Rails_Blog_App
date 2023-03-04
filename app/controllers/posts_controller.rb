55fba585066f322d6e4af12d7024d57215eabcf7
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-04 19:11:31 +0500

    Add new and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,33 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    author = current_user
+    @post.author = author
+    if @post.save
+      flash[:success] = 'Post successfully added!'
+      redirect_to user_posts_path(current_user, @post)
+    else
+      flash.now[:error] = 'Post creation failed!'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
