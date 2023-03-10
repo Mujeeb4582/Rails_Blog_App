a8c8b88d82bdc6dbbfd5494a48eceabbf84274d5
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-10 11:09:36 +0500

    Add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
+        #  :confirmable
   has_many :posts, inverse_of: :author, foreign_key: :author_id
   has_many :comments, inverse_of: :author, foreign_key: :author_id
   has_many :likes, inverse_of: :author, foreign_key: :author_id
 
   validates :name, presence: true
+
+  ROLES = %i[admin default].freeze
+
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
+
+  def admin?
+    is? :admin
+  end
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
