99bff672dc0da4bb39406c05fb94bbf03fdd1bea
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-10 11:12:34 +0500

    Add Alert message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: "You are not authorized to access this page."
+  end
+
+
   protected
 
   def update_allowed_parameters
