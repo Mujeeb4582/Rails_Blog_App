eb958fac3e56a41c30c20efb891aa20df4f2be26
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-11 22:08:37 +0500

    Add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :create, :destroy] do
+          resources :comments, only: [:index, :create, :destroy]
+        end
+      end
+    end
+  end
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show] do
