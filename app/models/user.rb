6988a33e4fcf1f0d4628184eeb0fc4925e0ddc40
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-12 20:00:07 +0500

    Add the validation for post_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :likes, inverse_of: :author, foreign_key: :author_id
 
   validates :name, presence: true
-
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   ROLES = %i[admin default].freeze
 
   def is?(requested_role)
